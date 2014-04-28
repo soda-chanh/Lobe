4f2fd3d9abb5873c048ebd92557f81903e16e988
Philip Gianfortoni <pwgianfortoni@gmail.com>
2014-04-27 20:53:48 -0400

    Added room editing.

--- src/lobe.ts
+++ src/lobe.ts
@@ -35,7 +35,9 @@ export class StagedObject {
     if (stage != null) {
       idx = stage.getChildIndex(oldObject);
       stage.removeChild(oldObject);
-      stage.addChildAt(this.stagedObject, idx);
+      if (object) {
+        stage.addChildAt(this.stagedObject, idx);
+      }
     }
     this.resetPos();
   }
@@ -43,17 +45,19 @@ export class StagedObject {
     this.restage(stage);
   }
   resetPos() {
-    this.stagedObject.x = this.stagePoint.x * tileW;
-    this.stagedObject.y = this.stagePoint.y * tileH;
+    if (this.stagedObject) {
+      this.stagedObject.x = this.stagePoint.x * tileW;
+      this.stagedObject.y = this.stagePoint.y * tileH;
+    }
   }
   restage(stage?: any):void {
     var oldStage:any;
+    oldStage = this.stagedObject.getStage();
     if (stage != null) {
-      oldStage = this.stagedObject.getStage();
-      if (oldStage != null) {
-        oldStage.removeChild(this.stagedObject);
-      }
       stage.addChild(this.stagedObject);
+    }
+    if (oldStage != null) {
+      oldStage.removeChild(this.stagedObject);
     }
     this.resetPos();
   }
@@ -61,7 +65,9 @@ export class StagedObject {
 export class Tile implements StagedObject {
   constructor(figure: Figure, x: number, y: number) {
     this.stagePoint = new Point(x, y);
-    this.stagedObject = figure.create();
+    if (figure != null) {
+      this.stagedObject = figure.create();
+    }
   }
   // StagedObject
   stagedObject: any; 
@@ -119,30 +125,110 @@ export class Dictionary<T> {
     return idx;
   }
 }
+export interface RoomFileInterface {
+  tiles: number[][];
+  mask: number[][];
+  start: Point;
+  finish: Point;
+}
+
+export class RoomFile {
+  tileLayer: number[][];
+  maskLayer: number[][]; 
+  start: Point;
+  finish: Point;
+  constructor(data?: any) {
+    for (var i:number=0; i < rows; i++) {
+      this.tileLayer[i] = [];
+      this.maskLayer[i] = [];
+      this.start = new Point(0, 0);
+      this.finish = new Point(0, 0);
+      for (var j: number=0; j < cols; j++) {
+        this.tileLayer[i][j] = 0;
+        this.maskLayer[i][j] = 0;
+      }
+    }
+    this.start = new Point(0,0);
+    this.finish = new Point(0,0);
+    if (data) {
+      if (data instanceof String) {
+        this.parseFromString(data);
+      } else {
+        this.parse(data);
+      }
+    }
+  }
+  parse(data: RoomFileInterface) {
+    for (var i: number=0; i < rows; i++) {
+      var tileRow:number[] = data.tiles[i] || [];
+      var maskRow:number[] = data.mask[i] || [];
+      for (var j: number=0; j < cols; j++) {
+        this.tileLayer[i][j] = tileRow[j] || 0;
+        this.maskLayer[i][j] = maskRow[j] || 0;
+      }
+    }
+    this.start.x = data.start.x || 0;
+    this.start.y = data.start.y || 0;
+    this.finish.x = data.finish.x || 0;
+    this.finish.y = data.finish.y || 0;
+  }
+  parseFromString(data: string) {
+    this.parse(JSON.parse(data));
+  }
+  serialize(): any {
+    var data:any = {};
+    data.tiles = [];
+    data.mask = [];
+    for (var i: number=0; i < rows; i++) {
+      data.tiles[i] = [];
+      data.mask[i] = [];
+      for (var j: number=0; j < cols; j++) {
+        data.tiles[i][j] = this.tileLayer[i][j];
+        data.mask[i][j] = this.maskLayer[i][j];
+      }
+    }
+    data.start = { x: this.start.x, y: this.start.y };
+    data.finish = { x: this.finish.x, y: this.finish.y};
+    return data;
+  }
+  serializeToString(): string {
+    return JSON.stringify(this.serialize(), null, 1);
+  }
+}
 
 export class Room {
   stage: any;
+  name: string;
   tiles: Tile[][];
   start: Point;
-  constructor(stage: any, defaultFigure: Figure) {
-    this.start = new Point(0, 0);
+  finish: Point;
+  mask: number[][];
+  roomFile: RoomFile;
+  constructor(stage: any, roomFile?: any) {
     this.stage = stage;
+    this.roomFile = new RoomFile(roomFile);
     this.tiles = [];
+    this.mask = [];
+    this.start = new Point(this.roomFile.start.x, this.roomFile.start.y);
+    this.finish = new Point(this.roomFile.finish.x, this.roomFile.finish.y);
     for (var i:number=0; i < rows; i++) {
       this.tiles[i] = [];
+      this.mask[i] = [];
       for (var j:number=0; j < cols; j++) {
-        var tile: Tile = new Tile(defaultFigure, j, i);
-        this.tiles[i][j] = tile;
-        tile.addToStage(stage);
+        var figureId = this.roomFile.tileLayer[i][j];
+        this.tiles[i][j] = new Tile(figures.values[figureId], j, i);
+        this.tiles[i][j].restage(stage);
+        this.mask[i][j] = this.roomFile.maskLayer[i][j];
       }
     }
   }
   isMasked(x: number, y: number): boolean {
-    return false;
+    return this.mask[y][x] != 0;
   }
   onSuccessfulMove: (oldPoint: Point) => void;
   onWallMove: (attemptedPoint: Point) => void;
   onOutOfBoundsMove: (attemptedPoint: Point) => void;
+  onEnterFinishMove: (oldPoint: Point) => void;
   tileAt(x: number, y: number): Tile {
     return this.tiles[y][x];
   }
@@ -151,11 +237,133 @@ export class Room {
         tile.updateFigure(figure);
       });
   }
+  visitTilesXY(f: (tile: Tile, x: number, y: number) => any): void {
+    for (var i:number=0; i < rows; i++) {
+      for (var j:number=0; j < cols; j++) {
+        f(this.tiles[i][j], j, i);
+      }
+    }
+  }
   visitTiles(f: (tile: Tile) => any): void {
     for (var i:number=0; i < rows; i++) {
       for (var j:number=0; j < cols; j++) {
         f(this.tiles[i][j]);
       }
+    }
+  }
+}
+var editor: Editor;
+export function toggleEditMode() {
+  if (editMode) {
+    editor.enterEditMode();
+  } else {
+    editor.leaveEditMode();
+  }
+}
+
+var editMode: boolean;
+
+export class Editor {
+  constructor() {
+    editMode = false;
+    this.maskTiles = [];
+    this.savedFigures = [];
+    for (var i: number = 0; i < cols; i++) {
+      this.savedFigures[i] = [];
+      this.maskTiles[i] = [];
+      for (var j: number = 0; j < cols; j++) {
+        this.maskTiles[i][j] = new Tile(figures.get("darkFloor"), j, i);
+      }
+    }
+    this.maskAlpha = .5;
+    this.replaceRoom = true;
+  }
+  replaceRoom: boolean;
+  maskAlpha: number;
+  maskTiles: Tile[][];
+  savedFigures: any[][];
+  enterEditMode() {
+    editMode = true;
+    if (!this.replaceRoom) {
+      player.room.visitTilesXY((tile: Tile, x: number, y: number) => {
+          this.savedFigures[y][x] = tile.stagedObject;
+        });
+    }
+    this.refreshRoom();
+  }
+  unstageTiles() {
+    for (var i: number; i < rows; i++) {
+      for (var j: number; j < rows; j++) {
+        this.maskTiles[i][j].restage(null);
+      }
+    }
+  }
+  leaveEditMode() {
+    this.unstageTiles();
+    player.room.visitTilesXY((tile: Tile, x: number, y: number) => {
+        if (this.replaceRoom) {
+          player.room.masks[y][x] = player.room.roomFile.maskLayer[y][x];
+        } else {
+          tile.replaceObject(this.savedFigures[y][x]);
+        }
+      });
+    editMode = false;
+  }
+  placeTileAt(figureName: string, x: number, y: number) {
+    var tile:Tile = player.room.tileAt(x, y);
+    tile.updateFigure(figures.get(figureName));
+    player.room.roomFile.tileLayer[y][x] = figures.indexOf(figureName);
+  }
+  placeTile(figureName: string) {
+    this.placeTileAt(figureName, player.point.x, player.point.y);
+  }
+  toggleMask() {
+    var roomFile: RoomFile = player.room.roomFile;
+    var x: number = player.point.x;
+    var y: number = player.point.y;
+    var maskTile: Tile = this.maskTiles[y][x];
+    if (roomFile.maskLayer[y][x] == 0) {
+      var tile:Tile = player.room.tileAt(x, y);
+      roomFile.maskLayer[y][x] = 1;
+      maskTile.stagedObject.alpha = this.maskAlpha;
+    } else {
+      roomFile.maskLayer[y][x] = 0;
+      maskTile.stagedObject.alpha = 0;
+    }
+  }
+  refreshRoom() {
+    player.room.visitTilesXY((tile: Tile, x: number, y: number) => {
+        tile.updateFigure(figures.values[player.room.roomFile.tileLayer[y][x]]);
+      });
+    for (var i: number; i < rows; i++) {
+      for (var j: number; j < rows; j++) {
+        if (player.room.roomFile.maskLayer[i][j] == 0) {
+          this.maskTiles[i][j].stagedObject.alpha = 0;
+        } else {
+          this.maskTiles[i][j].stagedObject.alpha = .5;
+        }
+        this.maskTiles[i][j].restage(player.room.stage);
+      }
+    }
+  }
+  placeStart() {
+    player.room.roomFile.start.x = player.point.x;
+    player.room.roomFile.start.y = player.point.y;
+  }
+  placeFinish() {
+    player.room.roomFile.finish.x = player.point.x;
+    player.room.roomFile.finish.y = player.point.y;
+  }
+  saveRoomToJson():any {
+    return player.room.roomFile.serialize();
+  }
+  saveRoomToString(): string {
+    return player.room.roomFile.serializeToString();
+  }
+  loadRoom(data: any) {
+    if (editMode) {
+      player.room.roomFile.parseFrom(data);
+      this.refreshRoom();
     }
   }
 }
@@ -176,17 +384,26 @@ export class Player implements StagedObject {
   point: Point;
   moveTo(x: number, y: number) {
     if (x >= 0 && x < cols && y >= 0 && y < rows) {
-      if (this.room.isMasked(x, y)) {
-        this.room.onWallMove(new Point(x, y));
+      if (!editMode && this.room.isMasked(x, y)) {
+        if (!editMode) {
+          this.room.onWallMove(new Point(x, y));
+        }
       } else {
         var p:Point = new Point(this.point.x, this.point.y);
         this.point.x = x;
         this.point.y = y;
-        this.room.onSuccessfulMove(p);
+        if (!editMode) {
+          this.room.onSuccessfulMove(p);
+        }
         this.posAt(x, y);
+        if (x == this.room.finish.x && y == this.room.finish.y) {
+          this.room.onEnterFinishMove(p);
+        }
       }
     } else {
-      this.room.onOutOfBoundsMove(new Point(x, y));
+      if (!editMode) {
+        this.room.onOutOfBoundsMove(new Point(x, y));
+      }
     }
   }
   moveLeft() {
